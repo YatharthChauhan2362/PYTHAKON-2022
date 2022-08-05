1b4f43629663948c2606682edb1e9d4401e51ab7
YatharthChuahan2362 <yc232326@gmail.com>
2022-08-05 11:56:54 +0530

    Import Libraries

---
+++ File Automaton.py
@@ -0,0 +1,16 @@
+'''
+PYTHAKON 2022
+
+TEAM MEMBERS:
+Yatharth Chauhan
+Mehul Patel
+Reeya Thanki
+
+Project: File Organization With Automation Using Python
+'''
+# import os
+# import glob
+from tkinter import *
+from threading import *
+from PIL import ImageTk, Image
+from tkinter import messagebox, ttk, filedialog
